64eaf7a9423f32d6acd82dee1370f5fa138ee379
KalanchoeChrysanthemum <157724777+KalanchoeChrysanthemum@users.noreply.github.com>
2025-02-28 19:52:08 -0500

    Convert hello world to utilize getopts (example)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let ref _prog = args[0];
+
+    let mut opts = Options::new();
+
+    // Opt flags
+    opts.optflag("h", "hello", "prints hello world");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!("{}", f.to_string());
+        }
+    };
+
+    if matches.opt_present("h") {
+        println!("Hello, World!");
+    }
 }
