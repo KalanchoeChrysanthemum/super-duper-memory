1a4e1d6c2fd13817218e8e79700ba7ec6a0a11c1
ReeseHatfield <hatfield.69@wright.edu>
2025-03-03 13:38:29 -0500

    parallel builds

--- scripts/build_benchmarks.sh
+++ scripts/build_benchmarks.sh
@@ -11,14 +11,31 @@ rm -rf build-tmp/*
 # I just want the executable, so i just put them here and mv them over
 # think I can just use cargo clean on that build dir later too
 
-
+# 22 seconds with 5 bins pre optimization
+# 14 seconds with 5 bins post optimization
 build_and_cp_exe(){
     CUR="$1"
-    cargo build --release --target-dir build-tmp --bin "${CUR}"
-    mv "build-tmp/release/${CUR}" bin
-    cargo clean --target-dir build-tmp
+    # this target build dir has to be unique for this to run in parallel
+    TMP_TARGET="${CUR}-build"
+    mkdir -p "build-tmp/${TMP_TARGET}"
 
+    # this does *not* compile them all mains in parallel, but compiles 
+    # each main itself (mostly its dependencies) in parallel, so it
+    # should be proportional to the number of mains we have 
+    # NUM_WORKERS=$(ls src/bin/benchmarks | wc -l) 
+    # NUM_WORKERS=$(nproc)
+
+    echo "Compiling ${CUR}..."
+    # parallel build doesn't seem to help honestly, but running this func in par. does
+    cargo build -j $(nproc) --release --target-dir "build-tmp/${TMP_TARGET}" --bin "${CUR}"
+    
+    
+    mv "build-tmp/${TMP_TARGET}/release/${CUR}" bin
+
+
+    rm -rf "build-tmp/${TMP_TARGET}"  
 }
+
 
 for file in src/bin/benchmarks/*; do
 
@@ -27,9 +44,12 @@ for file in src/bin/benchmarks/*; do
     exe_name="${filename%.*}"
 
     echo "Found executable: $exe_name"
-    build_and_cp_exe $exe_name
+    # run in child
+    build_and_cp_exe $exe_name &
 done
 
+# need children to finish before this we cleanup
+wait
 
 rm -rf build-tmp
 
