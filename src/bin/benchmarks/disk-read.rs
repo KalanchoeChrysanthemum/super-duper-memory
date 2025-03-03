76c7bc57b439c565541ce73517d997baa13b0400
ReeseHatfield <hatfield.69@wright.edu>
2025-03-03 13:38:43 -0500

    disk to disk read and write

---
+++ src/bin/benchmarks/disk-read.rs
@@ -0,0 +1,19 @@
+use std::{fs, io::{BufReader, Read}};
+
+pub fn main(){
+    println!("Running disk-read benchmark");
+
+    // im just using dev random, because I didn't wanna have to write to a new file first
+    // (also tossed around just using the side effect from disk-write)
+    let path = "/dev/random";
+
+    let file = fs::File::open(path).expect("Couldn't open the /dev/random");
+
+    let mut reader = BufReader::new(file);
+    // read 3 gigs from fs into memory
+    let mut buffer = vec![0; 1024 * 1024 * 1024 * 3]; 
+
+    reader.read_exact(&mut buffer).expect("Could not read bytes");
+    println!("Finished disk-read benchmark");
+
+}
