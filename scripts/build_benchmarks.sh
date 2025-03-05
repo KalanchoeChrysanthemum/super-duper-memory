a206bd256a84d4cf259b9d56cf40d39c7e68c2a3
ReeseHatfield <hatfield.69@wright.edu>
2025-03-04 21:32:46 -0500

    optional gpu benchmark

--- scripts/build_benchmarks.sh
+++ scripts/build_benchmarks.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 # crazy parallel bash build script :0
 
-# make if not exist and clean build dirs
-mkdir -p bin
-rm -rf bin/*
+EXCLUDE_GPU=false
 
-mkdir -p build-tmp
-rm -rf build-tmp/*
-# cargo build, even with release flag, puts a bunch of random shit in the build dir
-# I just want the executable, so i just put them here and mv them over
-# think I can just use cargo clean on that build dir later too
+usage(){
+    echo "./scripts/build_benchmarks.sh [FLAGS]"
+    echo "    [--no-gpu] Exclude GPU benchmark from compilation. Shaves off about several of compile time"
+    echo "    [--help] print this menu"
+}   
 
-# 22 seconds with 5 bins pre optimization
-# 14 seconds with 5 bins post optimization
 build_and_cp_exe(){
     CUR="$1"
+
+    echo "cur $CUR"
+
+    # we need to pass the gpu flag for conditional compilation
+    FEATURES="--features gpu"
+    [[ "$1" == "--no-gpu" ]] && FEATURES=""
+
+
+    # just not compiling the binary with the executable is not enough
+    # bc the wgpu is in the dependency tree for the cargo toml
+    if [[ "$CUR" == "gpu" && "$EXCLUDE_GPU" == "true" ]]; then
+        echo "Skipping GPU benchmark compilation..."
+        return 0  
+    fi
+
     # this target build dir has to be unique for this to run in parallel
     TMP_TARGET="${CUR}-build"
     mkdir -p "build-tmp/${TMP_TARGET}"
@@ -27,14 +38,40 @@ build_and_cp_exe(){
 
     echo "Compiling ${CUR}..."
     # parallel build doesn't seem to help honestly, but running this func in par. does
-    cargo build -j $(nproc) --release --target-dir "build-tmp/${TMP_TARGET}" --bin "${CUR}"
-    
+    # cargo build -j $(nproc) --release --target-dir "build-tmp/${TMP_TARGET}" --bin "${CUR}"
+    if [[ "$CUR" == "gpu" ]]; then
+        cargo build -j $(nproc) --release --target-dir "build-tmp/${TMP_TARGET}" --bin "${CUR}" --features gpu
+    else
+        cargo build -j $(nproc) --release --target-dir "build-tmp/${TMP_TARGET}" --bin "${CUR}"
+    fi
+
     
     mv "build-tmp/${TMP_TARGET}/release/${CUR}" bin
 
 
     rm -rf "build-tmp/${TMP_TARGET}"  
 }
+
+if [[ "$1" == "--help" ]]; then 
+    usage
+    exit 0
+fi
+
+
+if [[ "$1" == "--no-gpu" ]]; then
+    EXCLUDE_GPU=true
+fi
+
+# make if not exist and clean build dirs
+# but do NOT clean up gpu bin
+mkdir -p bin
+find bin -mindepth 1 ! -name 'gpu' -delete
+
+mkdir -p build-tmp
+rm -rf build-tmp/*
+# cargo build, even with release flag, puts a bunch of random shit in the build dir
+# I just want the executable, so i just put them here and mv them over
+# think I can just use cargo clean on that build dir later too
 
 
 for file in src/bin/benchmarks/*; do
